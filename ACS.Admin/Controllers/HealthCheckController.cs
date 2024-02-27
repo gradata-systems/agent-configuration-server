562a8590873279d385e515a85e4a1bb2c4d38fdc
Peter Kimberley <peter@gradata.com.au>
2024-02-27 20:50:12 +1100

    Add healthcheck

---
+++ ACS.Admin/Controllers/HealthCheckController.cs
@@ -0,0 +1,23 @@
+﻿using ACS.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ACS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get(AppDbContext dbContext)
+        {
+            if (await dbContext.Database.CanConnectAsync())
+            {
+                return Ok();
+            }
+            else
+            {
+                return Problem("Failed to connect to database");
+            }
+        }
+    }
+}
